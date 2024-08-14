811e01ef280529ba29ee78fb042c7cfce2c42a4e
Mark Lavercombe <mlava@users.noreply.github.com>
2024-08-14 15:31:39 +1000

    update

--- README.md
+++ README.md
@@ -10,18 +10,16 @@ It will automatically recognise:
 
 - YouTube videos for {{youtube: url}}
 - Vimeo videos for {{[[video]]: url}}
+- Tiktok videos
 - Figma diagrams for{{figma: url}}
 - PDFs for {{pdf: url}}
 - websites (for iframe embed) for {{iframe: url}}
 - images for ![](url)
   - jpg|jpeg|bmp|gif|png|tiff|webp
+- Instagram images
 - audio files for {{[[audio]]: url}}
   - mp3|wav|aiff|aac|ogg|wma|flac|alac
 - video files for {{[[video]]: url}}
   - avi|mpg|mpeg|mov|mkv|mp4|wmv|webm
 
 Let me know if there are other formats that need to be added. I can also update if Roam Research adds more {{ shortcodes }}.
-
-TODO:
-
-~~1. implement a keyboard shortcut in addition to the Command Palette command.~~
--- extension.js
+++ extension.js
@@ -14,10 +14,12 @@ export default {
                 let q = `[:find (pull ?page [:node/title :block/string :block/uid ]) :where [?page :block/uid "${startBlock}"]  ]`;
                 var info = await window.roamAlphaAPI.q(q);
             }
-            
+
             const videoRegex = /^https?:\/\/(.+\/)+.+(\.(avi|mpg|mpeg|mov|mkv|mp4|wmv|webm))$/;
             const vimeoRegex = /^(http|https)?:\/\/(www\.)?vimeo.com.+/;
             const imageRegex = /^https?:\/\/(.+\/)+.+(\.(jpg|jpeg|bmp|gif|png|tiff|webp))$/;
+            const instagramRegex = /\.+instagram\.com.*?\/p\/(.*)\//;
+            const tiktokRegex = /\.+tiktok\.com.*?\/video\/(.*)\/?/;
             const audioRegex = /^https?:\/\/(.+\/)+.+(\.(mp3|wav|aiff|aac|ogg|wma|flac|alac))$/;
             const figmaRegex = /https:\/\/([\w\.-]+\.)?figma.com\/(file|proto)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/;
             const websiteRegex = /(https?:\/\/)?(www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|(https?:\/\/)?(www\.)?(?!ww)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
@@ -39,8 +41,14 @@ export default {
                     embedString = "{{[[video]]: " + clipText + "}}";
                 } else if (videoRegex.test(clipText)) { // video but not youtube or vimeo
                     embedString = "{{[[video]]: " + clipText + "}}";
+                } else if (tiktokRegex.test(clipText)) { // video but not youtube or vimeo
+                    let code = clipText.match(tiktokRegex);
+                    embedString = "{{[[iframe]]: https://www.tiktok.com/player/v1/" + code[1] + "?autoplay=0}}";
                 } else if (imageRegex.test(clipText)) { // image
                     embedString = "![](" + clipText + ")";
+                } else if (instagramRegex.test(clipText)) { // instagram
+                    let code = clipText.match(instagramRegex);
+                    embedString = "{{[[iframe]]: https://www.instagram.com/p/" + code[1] + "/embed}}";
                 } else if (audioRegex.test(clipText)) { // audio
                     embedString = "{{[[audio]]: " + clipText + "}}";
                 } else if (clipText.match(/^https?:\/\/(.+\/)+.+(\.(pdf))$/)) { // pdf
@@ -57,6 +65,8 @@ export default {
             if (info.length > 0 && embedState == true) {
                 await window.roamAlphaAPI.updateBlock(
                     { block: { uid: info[0][0].uid, string: embedString.toString(), open: true } });
+                await sleep(50);
+                document.querySelector("body")?.click();
             }
         }
     },
@@ -68,4 +78,7 @@ export default {
 function isUrl(s) {
     var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
     return regexp.test(s);
+}
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
